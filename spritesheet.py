e2b3215b0070c7c95bf1f84ab9564efb10fcea99
bombeuler <bombeuler@163.com>
2022-09-06 10:28:56 +0800

    修复了spritesheet背景不透明的bug

--- spritesheet.py
+++ spritesheet.py
@@ -1,22 +1,26 @@
 import pygame
 from jsonconvert import JSONConvert
 from config import *
-
+from pygame.locals import SRCALPHA
+from debug import debug
 
 class SpriteSheet:
     def __init__(self, sheetName):
         sheetPath = f'{ASSETS_PATH}/{sheetName}.png'
-        self.sheet = pygame.image.load(sheetPath).convert_alpha()
+        sheetImage = pygame.image.load(sheetPath).convert_alpha()
+        # sheetImage.set_colorkey(pygame.Color(0,0,0,255))
+        self.sheet = sheetImage
         self.data = JSONConvert(sheetName)
         # print(f'无法加载图片{sheetName}')
         # print(pygame.error)
 
     def __cut_one(self, rect, scale):
         rect = pygame.Rect(rect)
-        image = pygame.Surface(rect.size).convert_alpha()
+        image = pygame.Surface(rect.size,SRCALPHA)
         image.blit(self.sheet, (0, 0), rect)
         (_, _, w, h) = rect
-        image = pygame.transform.scale(image, (scale * w, scale * h))
+        image = pygame.transform.scale(image,
+                                       (scale * w, scale * h))
         return image
 
     def loop_img(self, name, scale=2):
